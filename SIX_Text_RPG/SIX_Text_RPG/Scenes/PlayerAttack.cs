a744c83153baf920a14ce736319084ebe45e16b7
sungmars <136216542+sungmars@users.noreply.github.com>
2025-02-07 16:33:40 +0900

    <feat> 플레이어 정보 출력
    플레이어 정보를 받아서 출력하는 코드 추가

--- SIX_Text_RPG/SIX_Text_RPG/Scenes/PlayerAttack.cs
+++ SIX_Text_RPG/SIX_Text_RPG/Scenes/PlayerAttack.cs
@@ -7,7 +7,7 @@ namespace SIX_Text_RPG.Scenes
     internal class Scene_PlayerAttack : Scene_Base
     {
         private List<Monster> monsters;
-        
+        Player? player = GameManager.Instance.Player;
         private static Random random = new Random();
         //Monster 리스트를 전달받는 생성자
         public Scene_PlayerAttack(List<Monster> monsters)
@@ -21,6 +21,10 @@ namespace SIX_Text_RPG.Scenes
             sceneTitle = "Battle!!";
             sceneInfo = "";
         }
+        public override int Update()
+        {
+            return base.Update();
+        }
 
         // Monster 리스트 출력
         protected override void Display()
@@ -28,13 +32,23 @@ namespace SIX_Text_RPG.Scenes
             for (int i = 0; i < monsters.Count; i++)
             {
                 // 번호를 붙여 출력
-                Console.Write($"{i + 1}. \n");
+                Console.Write($"{i + 1}. ");
                 if (monsters[i].IsDead)
                 {
                     Console.ForegroundColor = ConsoleColor.DarkGray;
                 }
                 monsters[i].DisplayMonster();
                 Console.ResetColor();
+                
+                
+            }
+            if (player == null)
+            {
+                Console.WriteLine("\n플레이어 정보를 찾을 수 없습니다.");
+            }
+            else
+            {
+                player.DisplayInfo();
             }
         }
         
