b60ab934f05fc03f75abfcca546a690aa07793c0
SpartaCodingClub <spartacodingclub.kr@gmail.com>
2025-02-07 14:15:30 +0900

    <Feat> 플레이어 정보 출력
    전투씬에서 사용될 상태바 출력과
    상점씬에서 사용될 소지금 출력이
    구분되어 있습니다.

--- SIX_Text_RPG/SIX_Text_RPG/Creature/Player.cs
+++ SIX_Text_RPG/SIX_Text_RPG/Creature/Player.cs
@@ -15,5 +15,57 @@
             stats.Gold += gold;
             Stats = stats;
         }
+
+        public void DisplayInfo()
+        {
+            DisplayInfo_Status();
+
+            Console.WriteLine($" 공격력: {Stats.ATK}");
+            Console.WriteLine($" 방어력: {Stats.DEF}\n");
+
+            DisplayInfo_Gold();
+        }
+
+        public void DisplayInfo_Status()
+        {
+            Utils.WriteColorLine($" Lv.{Stats.Level:00}", ConsoleColor.White);
+            Utils.WriteColor($" {Stats.Name}", ConsoleColor.DarkYellow);
+            Console.WriteLine($" {Type}\n", ConsoleColor.DarkGray);
+
+            Console.Write($" 경험치: ");
+            Display_StatusBar(Stats.HP, Stats.MaxHP, ConsoleColor.DarkGreen);
+            Console.Write($" 체  력: ");
+            Display_StatusBar(Stats.HP, Stats.MaxHP, ConsoleColor.Red);
+            Console.Write($" 마  력: ");
+            Display_StatusBar(Stats.HP, Stats.MaxHP, ConsoleColor.Blue);
+            Console.WriteLine();
+        }
+
+        private void Display_StatusBar(float value, float maxValue, ConsoleColor color)
+        {
+            (int left, int top) = Console.GetCursorPosition();
+
+            Utils.WriteColor("[][][][][][][][][][]", ConsoleColor.DarkGray);
+            Console.WriteLine($" {value}/{maxValue}", ConsoleColor.White);
+
+            int unit = IsDead ? 0 : (int)MathF.Max(Stats.HP / Stats.MaxHP * 20, 1);
+            while (unit > 0)
+            {
+                Console.SetCursorPosition(left, top);
+                if (left % 2 == 1) Utils.WriteColor("[", color);
+                else Utils.WriteColor("]", color);
+
+                left++;
+                unit--;
+            }
+
+            Console.SetCursorPosition(0, top + 1);
+        }
+
+        public void DisplayInfo_Gold()
+        {
+            Console.Write($" 소지금:");
+            Utils.WriteColorLine($" {Stats.Gold}G", ConsoleColor.Yellow);
+        }
     }
 }
---
+++ SIX_Text_RPG/SIX_Text_RPG/Scenes/Scene_PlayerInfo.cs
@@ -0,0 +1,35 @@
+﻿namespace SIX_Text_RPG.Scenes
+{
+    internal class Scene_PlayerInfo : Scene_Base
+    {
+        public override void Awake()
+        {
+            base.Awake();
+
+            sceneTitle = "상태 보기";
+            sceneInfo = "캐릭터의 정보가 표시됩니다.";
+        }
+
+        public override int Update()
+        {
+            switch (base.Update())
+            {
+                case 0:
+                    Program.CurrentScene = new Scene_Title();
+                    break;
+            }
+
+            return 0;
+        }
+
+        protected override void Display()
+        {
+            if (GameManager.Instance.Player == null)
+            {
+                return;
+            }
+
+            GameManager.Instance.Player.DisplayInfo();
+        }
+    }
+}
