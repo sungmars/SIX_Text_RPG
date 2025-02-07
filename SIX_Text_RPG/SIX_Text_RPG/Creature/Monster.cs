979dad846f8c6f2c09b7ee4cd156615642701c59
kimdhmath <kimdhmath0@gmail.com>
2025-02-07 10:58:40 +0900

    <feat>배틀 시작, 몬스터 클래스
    Scene_BattleStart에서 몬스터 생성 및 출력, Monster에서 몬스터 스탯 출력

--- SIX_Text_RPG/SIX_Text_RPG/Creature/Monster.cs
+++ SIX_Text_RPG/SIX_Text_RPG/Creature/Monster.cs
@@ -8,5 +8,21 @@
     internal class Monster : Creature
     {
         public MonsterType Type { get; private set; }
+        public Monster(MonsterType type, string name, int level, float atk, float def, float hp, int gold) : base(name, level, atk, def, hp, gold)
+        {
+            Type = type;
+        }
+
+        public void DisplayMonster()
+        {
+            if (IsDead)
+            {
+                Console.WriteLine($"Lv.{Stats.Level} {Stats.Name} Dead");
+            }
+            else
+            {
+                Console.WriteLine($"Lv.{Stats.Level} {Stats.Name}  HP {Stats.HP}");
+            }
+        }
     }
 }
---
+++ SIX_Text_RPG/SIX_Text_RPG/Scenes/Scene_BattleStart.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIX_Text_RPG.Scenes
+{
+    internal class Scene_BattleMenu : Scene_Base
+    {
+        private Random random = new Random();
+        private List<Monster> monsters = new List<Monster>();
+        public override void Awake()
+        {
+            base.Awake();
+            sceneTitle = "Battle!!";
+            sceneInfo = "";
+
+            Menu.Add("공격");
+            MakeMonster();
+        }
+
+        public override int Update()
+        {
+            switch (base.Update())
+            {
+                case 1:
+                    Program.CurrentScene = new Scene_BattleMenu();
+                    return 0;
+                default:
+                    Program.CurrentScene = new Scene_BattleMenu();
+                    return 0;
+            }
+        }
+
+        protected override void Display()
+        {
+            foreach (var monster in monsters)
+            {
+                monster.DisplayMonster();
+            }
+        }
+
+        private void MakeMonster()
+        {
+            int monsterCount = random.Next(1, 5);
+            for (int i = 0; i < monsterCount; i++)
+            {
+                monsters.Add(SetMonster(random.Next(1, 4)));
+            }
+        }
+
+        private Monster SetMonster(int index)
+        {
+            switch (index)
+            {
+                case 1:
+                    return new Monster(MonsterType.None, "슬라임", 1, 10, 5, 10, 0);
+                case 2:
+                    return new Monster(MonsterType.None, "고블린", 2, 20, 10, 15, 0);
+                case 3:
+                    return new Monster(MonsterType.None, "오크", 3, 30, 15, 20, 0);
+                default:
+                    return new Monster(MonsterType.None, "버그", 100, 10000, 100, 100, 0);
+            }
+        }
+
+        public List<Monster> GetMonsters()
+        {
+            return monsters;
+        }
+    }
+}
