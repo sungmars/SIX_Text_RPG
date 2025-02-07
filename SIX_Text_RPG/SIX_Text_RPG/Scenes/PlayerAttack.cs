5041daf373d44cc188c5802c3477a09d72b19a64
sungmars <136216542+sungmars@users.noreply.github.com>
2025-02-07 16:05:08 +0900

    <feat>몬스터 리스트 출력
    몬스터 리스트를 받아서 출력하는 코드

---
+++ SIX_Text_RPG/SIX_Text_RPG/Scenes/PlayerAttack.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace SIX_Text_RPG.Scenes
+{
+    internal class Scene_PlayerAttack : Scene_Base
+    {
+        private List<Monster> monsters;
+        
+        private static Random random = new Random();
+        //Monster 리스트를 전달받는 생성자
+        public Scene_PlayerAttack(List<Monster> monsters)
+        {
+            this.monsters = monsters;
+        }
+
+        public override void Awake()
+        {
+            base.Awake();
+            sceneTitle = "Battle!!";
+            sceneInfo = "";
+        }
+
+        // Monster 리스트 출력
+        protected override void Display()
+        {
+            for (int i = 0; i < monsters.Count; i++)
+            {
+                // 번호를 붙여 출력
+                Console.Write($"{i + 1}. \n");
+                if (monsters[i].IsDead)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                }
+                monsters[i].DisplayMonster();
+                Console.ResetColor();
+            }
+        }
+        
+    }
+}
