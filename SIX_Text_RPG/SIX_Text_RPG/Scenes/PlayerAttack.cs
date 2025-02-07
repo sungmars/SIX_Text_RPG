c0b77b3101e29452a5a9aae197bb18a587943c9c
sungmars <136216542+sungmars@users.noreply.github.com>
2025-02-07 17:20:01 +0900

    <feat>플레이어 공격기능 추가
    공격하면 "잘못된 입력이다"라고 출력됨.
    우선 develop내용을 merge받기 위해 커밋

--- SIX_Text_RPG/SIX_Text_RPG/Scenes/PlayerAttack.cs
+++ SIX_Text_RPG/SIX_Text_RPG/Scenes/PlayerAttack.cs
@@ -9,6 +9,7 @@ namespace SIX_Text_RPG.Scenes
         private List<Monster> monsters;
         Player? player = GameManager.Instance.Player;
         private static Random random = new Random();
+
         //Monster 리스트를 전달받는 생성자
         public Scene_PlayerAttack(List<Monster> monsters)
         {
@@ -20,11 +21,9 @@ namespace SIX_Text_RPG.Scenes
             base.Awake();
             sceneTitle = "Battle!!";
             sceneInfo = "";
+
         }
-        public override int Update()
-        {
-            return base.Update();
-        }
+       
 
         // Monster 리스트 출력
         protected override void Display()
@@ -48,9 +47,80 @@ namespace SIX_Text_RPG.Scenes
             }
             else
             {
-                player.DisplayInfo();
+                player.DisplayInfo_Status();
             }
+
         }
+        private int CalculateDamage(float baseATK)
+        {
+            int errorMargin = (int)MathF.Ceiling(baseATK * 0.1f);
+            int minDamage = (int)baseATK - errorMargin;
+            int maxDamage = (int)baseATK + errorMargin;
+            if (minDamage < 0) minDamage = 0;
+            return random.Next(minDamage, maxDamage + 1);
+        }
+        public override int Update()
+        {
+            int selection = -1;
+            bool validSelection = false;
+
+            do
+            {
+                Console.Write("대상을 선택해주세요.");
+                selection = Utils.ReadIndex(hasZero);
+
+                if (selection == 0)
+                {
+                    return 0;
+                }
+                if (selection < 1 || selection > monsters.Count)
+                {
+                    Utils.WriteColorLine("잘못된 입력입니다", ConsoleColor.Red);
+                    continue;
+                }
+                Monster target = monsters[selection - 1];
+                if (target.IsDead)
+                {
+                    Utils.WriteColorLine("잘못된 입력입니다", ConsoleColor.Red);
+                    continue;
+                }
+                validSelection = true;
+            } while (!validSelection);
+
+            Monster monsterToAttack = monsters[selection - 1];
+            float previousHP = monsterToAttack.Stats.HP;
+            int damage = CalculateDamage(player.Stats.ATK);
+            monsterToAttack.Damaged(damage);
+
+            Console.Clear();
+            Utils.WriteColorLine("Battle!!\n", ConsoleColor.DarkYellow);
+            Utils.WriteColorLine($"{player.Stats.Name} 의 공격!", ConsoleColor.White);
+            Utils.WriteColorLine(
+                $"Lv.{monsterToAttack.Stats.Level} {monsterToAttack.Stats.Name} 을(를) 맞췄습니다. [데미지 : {damage}]\n",
+                ConsoleColor.White);
+
+            Console.Write($"Lv.{monsterToAttack.Stats.Level} {monsterToAttack.Stats.Name}\nHP {previousHP} -> ");
+            if (monsterToAttack.IsDead)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.Write("Dead");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.Write(monsterToAttack.Stats.HP);
+            }
+            Console.WriteLine("\n");
+            Utils.WriteColorLine("0. 다음", ConsoleColor.White);
+            
+            while (Console.ReadLine() != "0") { }
+
+            
+
+
+            return selection;
         
+
+        }
     }
 }
