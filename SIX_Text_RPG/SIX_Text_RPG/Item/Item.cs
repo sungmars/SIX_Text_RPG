5434319cc926450dfcf28f0c96c83443f76d6ce1
hannah896 <phn2260@naver.com>
2025-02-08 18:24:49 +0900

    🔨<Refactor>: “Item 클래스 구조 개선 및 인자 추가"
    item의 값을 가져오려면 Iteminfo로 접근하면 됩니다.
    필요 없는 enum을 삭제 했습니다.

--- SIX_Text_RPG/SIX_Text_RPG/Item/Item.cs
+++ SIX_Text_RPG/SIX_Text_RPG/Item/Item.cs
@@ -1,34 +1,40 @@
-﻿namespace SIX_Text_RPG.Others
+﻿using System.Numerics;
+
+namespace SIX_Text_RPG.Item
 {
-    public enum Type
-    {
-        Accessary,
-        Armor,
-        Bullet,
-        Potion
-    }
-
-    public enum Effect
-    {
-        ATK,
-        DEF,
-        HP
-    }
-
-    public struct itemInfo
+    public struct ItemInfo
     {
         public string Name { get; set; }
         public string Description { get; set; }
 
-        public bool IsSold { get; set; }
+        public float MaxHP { get; set; }
+        public float HP { get; set; }
+
         public int Price { get; set; }
+        public int ATK { get; set; }
+        public int DEF { get; set; }
+
+        public bool IsSold { get; set; }
     }
 
 
 
-    internal class Item
+    internal abstract class Item
     {
-        public 
-        public Item(string name) 
+        public ItemInfo Iteminfo { get; private set; }
+
+        public Item(string name, string desciption, float hp, int price, int atk, int def)
+        {
+            Iteminfo = new ItemInfo()
+            {
+                Name = name,
+                Description = desciption,
+                HP = hp,
+                Price = price,
+                ATK = atk,
+                DEF = def,
+                IsSold = false
+            };
+        }
     }
 }
