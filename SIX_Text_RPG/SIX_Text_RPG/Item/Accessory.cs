c0c618d75a90c58b26063a78474f5bbbf101c4bd
hannah896 <phn2260@naver.com>
2025-02-08 19:56:12 +0900

    🙈 <WIP>: “막세서리 아이템 작업중
    임시로 필요한 메서드작성만 해뒀습니다.

--- SIX_Text_RPG/SIX_Text_RPG/Item/Accessory.cs
+++ SIX_Text_RPG/SIX_Text_RPG/Item/Accessory.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace SIX_Text_RPG.Item
 {
-    internal class Accessory
+    internal class Accessory : Item, IEquipable
     {
+        public Accessory(string name, string desciption, float hp, int price, int atk, int def) : base(name, desciption, hp, price, atk, def)
+        {
+
+        }
+
+        void IEquipable.Equip()
+        {
+            if (GameManager.Instance.Player == null) return;
+
+            Player player = GameManager.Instance.Player;
+
+
+        }
     }
 }
