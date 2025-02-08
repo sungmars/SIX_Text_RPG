01655d439f8d6b6211a8980cd462b49ec81a4d11
hannah896 <phn2260@naver.com>
2025-02-08 18:34:48 +0900

    ✨<Feat>: “Armor, Weapon 클래스 임시 구현완료!”
    아이템을 장착했을 때 플레이어의 스텟이 아이템의 속성의 값만큼 증가하도록 했습니다.

--- SIX_Text_RPG/SIX_Text_RPG/Item/Armor.cs
+++ SIX_Text_RPG/SIX_Text_RPG/Item/Armor.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace SIX_Text_RPG.Item
 {
-    internal class Armor
+    internal class Armor : Item, IEquipable
     {
+        public Armor(string name, string desciption, float hp, int price, int atk, int def) : base(name, desciption, hp, price, atk, def)
+        {
+        }
+
+        void IEquipable.Equip()
+        {
+            if (GameManager.Instance.Player == null) return;
+
+            Player player = GameManager.Instance.Player;
+
+            player.SetStat(Stat.DEF, Iteminfo.DEF, true);
+            player.SetStat(Stat.ATK, Iteminfo.ATK, true);
+            player.SetStat(Stat.HP, Iteminfo.HP, true);
+            player.SetStat(Stat.MaxHP, Iteminfo.HP, true);
+        }
     }
 }
