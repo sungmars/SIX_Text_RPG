9a1b7529f23fa6b062c5e75b4c05f64c86eb0ad2
sungmars <136216542+sungmars@users.noreply.github.com>
2025-02-09 15:45:32 +0900

    ✨<Feat>: “ 장착관리없는 인벤토리 씬추가"
    장착관리기능 없는 인벤토리 씬 추가
    플레이어 어택씬은 주석처리됨

---
+++ SIX_Text_RPG/SIX_Text_RPG/Scenes/Scene_Inventory.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIX_Text_RPG.Scenes
+{
+    internal class Scene_Inventory:Scene_Base
+    {
+        private readonly List<Item> Inventory = new List<Item>();
+        private readonly int LEFT = 0;
+        private int cursorIndex;
+        private int cursorTop;
+        
+        public override void Awake()
+        {
+            base.Awake();
+            sceneTitle = "아이템 창";
+            sceneInfo = "";
+            hasZero = false;
+            Display();
+        }
+
+        public override int Update()
+        {
+            int cursor = LEFT - 3;
+            while (Program.CurrentScene == this)
+            {
+                var key =Console.ReadKey(true);
+                if (key.Key==ConsoleKey.UpArrow)
+                {
+                    Console.SetCursorPosition(cursor,cursorTop+cursorIndex);
+                    Console.Write(' ');
+
+                    cursorIndex = Math.Max(cursorIndex - 1, 0);
+                    Console.SetCursorPosition(cursor, cursorTop + cursorIndex);
+                    Utils.WriteColor("▶", ConsoleColor.DarkCyan);
+                }
+
+                if (key.Key == ConsoleKey.DownArrow)
+                {
+                    Console.SetCursorPosition(cursor, cursorTop + cursorIndex);
+                    Console.Write(' ');
+
+                    cursorIndex = Math.Min(cursorIndex + 1, Inventory.Count - 1);
+                    Console.SetCursorPosition(cursor, cursorTop + cursorIndex);
+                    Utils.WriteColor("▶", ConsoleColor.DarkCyan);
+                }
+                if (key.Key == ConsoleKey.Enter)
+                {
+                    ItemSelect(Inventory[cursorIndex]);
+
+                }
+            }
+            return 0;
+        }
+        protected override void Display()
+        {
+            Console.SetCursorPosition(0, 0);
+            //아이템 리스트 출력
+            cursorTop = Console.CursorTop + 1;
+            for(int i=0;i<Inventory.Count;i++)
+            {
+                Console.SetCursorPosition(LEFT, cursorTop+i);
+                if (i == cursorIndex)
+                {
+                    Utils.WriteColor("▶ ", ConsoleColor.DarkCyan);
+                }
+                else
+                {
+                    Console.Write("   ");
+                }
+                Utils.WriteColorLine(Inventory[i].Name,ConsoleColor.White);
+            }
+        }
+        private void ItemSelect(Item selctedItem)
+        {
+            Utils.WriteColorLine($"\n {아이템 프로퍼티.Name}가 선택되었습니다.",ConsoleColor.DarkYellow);
+        }
+
+    }
+}
