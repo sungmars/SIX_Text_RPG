cdc48013d80069bc7ed26836d692abcbc6d0def1
hannah896 <phn2260@naver.com>
2025-02-10 11:20:28 +0900

    ✨<Feat>: “아이템 모든 타입의 정보 Get기능 추가, 아이템 Bool타입 Set기능추가 ”
    타입에 맞게 GetStatFloat, GetStatInt, GetStatString, GetStatBool등 호출하면 아이템의 속성 값을 가져올 수 있습니다.
    
    SetBool을 통해 아이템의  bool타입 속성값 isEquip, isSold등에 접근이 가능합니다.

--- SIX_Text_RPG/SIX_Text_RPG/Item/Item.cs
+++ SIX_Text_RPG/SIX_Text_RPG/Item/Item.cs
@@ -3,6 +3,21 @@ using System.Numerics;
 
 ﻿namespace SIX_Text_RPG
 {
+    enum ItemStat
+    {
+        Name,
+        Description,
+        HP,
+        MaxHP,
+        MP,
+        MaxMP,
+        ATK,
+        DEF,
+        Price,
+        IsSold,
+        IsEquip
+    }
+
     public struct ItemInfo
     {
         public string Name { get; set; }
@@ -18,8 +33,8 @@ using System.Numerics;
         public int Price { get; set; }
 
         public bool IsSold { get; set; }
+        public bool IsEquip {  get; set; }
     }
-
 
 
     internal abstract class Item
@@ -39,8 +54,107 @@ using System.Numerics;
                 ATK = atk,
                 DEF = def,
                 Price = price,
-                IsSold = false
+                IsSold = false,
+                IsEquip = false
             };
+        }
+
+        //아이템의 bool타입 정보 수정하는 것
+        protected void SetBool(ItemStat itemstat)
+        {
+            ItemInfo temp = Iteminfo;
+            if (ItemStat.IsSold == itemstat)
+            {
+                temp.IsSold = true;
+                Iteminfo = temp;
+            }
+            else if (ItemStat.IsEquip == itemstat)
+            {
+                temp.IsEquip = true;
+                Iteminfo = temp;
+            }
+        }
+
+        //아이템의 string타입 정보 가져오기
+        public string GetStatString(ItemStat itemstat)
+        {
+            string value = "";
+            switch (itemstat)
+            {
+                case ItemStat.Name:
+                    value = Iteminfo.Name;
+                    break;
+
+                case ItemStat.Description:
+                    value = Iteminfo.Description;
+                    break;
+            }
+            return value;
+
+        }
+
+        //아이템의 int타입 정보 가져오기
+        public int GetStatInt(ItemStat itemstat)
+        {
+            int value = -1;
+            switch (itemstat)
+            {
+                case ItemStat.DEF:
+                    value = Iteminfo.DEF;
+                    break;
+
+                case ItemStat.ATK:
+                    value = Iteminfo.ATK;
+                    break;
+
+                case ItemStat.Price:
+                    value = Iteminfo.Price;
+                    break;
+                    
+            }
+            return value;
+        }
+
+        //아이템의 float타입 정보 가져오기
+        public float GetStatFloat(ItemStat itemstat)
+        {
+            float value = -999f;
+            switch (itemstat)
+            {
+                case ItemStat.MaxHP:
+                    value = Iteminfo.MaxHP;
+                    break;
+
+                case ItemStat.HP:
+                    value = Iteminfo.HP;
+                    break;
+
+                case ItemStat.MaxMP:
+                    value = Iteminfo.MaxMP;
+                    break;
+
+                case ItemStat.MP:
+                    value = Iteminfo.MP;
+                    break;
+            }
+            return value;
+        }
+
+        //아이템의 bool타입 정보 가져오기
+        public bool GetStatBool(ItemStat itemstat)
+        {
+            bool value = false;
+            switch (itemstat)
+            {
+                case ItemStat.IsSold:
+                    value = Iteminfo.IsSold;
+                    break;
+
+                case ItemStat.IsEquip:
+                    value = Iteminfo.IsEquip;
+                    break;
+            }
+            return value;
         }
     }
 }
