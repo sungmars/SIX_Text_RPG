884645ab945d4623156af83cbef47182c2a010e1
sungmars <136216542+sungmars@users.noreply.github.com>
2025-02-10 12:56:03 +0900

    🔨<Refactor>: “아이템클래스 메서드 사용”
    게임매니저에서 인벤토리정보를 가져오도록 변경했고 씬 설명과
    커서위치, 나가기 기능을 커서메뉴로 구현, 인벤토리 메뉴 for문의 람다식 기능 내용은 아이템 메서드를 사용하도록 변경//이 부분은 테스트 해봐야함

--- SIX_Text_RPG/SIX_Text_RPG/Scenes/Scene_Inventory.cs
+++ SIX_Text_RPG/SIX_Text_RPG/Scenes/Scene_Inventory.cs
@@ -8,120 +8,70 @@ using System.Threading.Tasks;
 
 namespace SIX_Text_RPG.Scenes
 {
+    
     internal class Scene_Inventory : Scene_Base
     {
-        private readonly List<Item> Inventory = new List<Item>();//이거 게임매니저로 갖고오게 해야함
-        
+        private List<Item> Inventory => GameManager.Instance.Inventory;
+
         public override void Awake()
         {
             base.Awake();
             sceneTitle = "인벤토리";
-            sceneInfo = "";
+            sceneInfo = "Enter키를 눌러 장착/해제 가능합니다.";
             hasZero = false;
-            MakeItem();
             
         }
 
         
         protected override void Display()
         {
-            //Console.SetCursorPosition(0, 0);
-            ////아이템 리스트 출력
-            //cursorTop = Console.CursorTop + 1;
-            //int totalmenu = Inventory.Count + 1;
-            //for (int i = 0; i < totalmenu; i++)//Count+1은 나가기 버튼
-            //{
-            //    Console.SetCursorPosition(LEFT, cursorTop + i);
-            //    if (i == cursorIndex)
-            //    {
-            //        Utils.WriteColor("▶", ConsoleColor.DarkCyan);
-            //    }
-            //    else
-            //    {
-            //        Console.Write("");
-            //    }
-            //    if (i < Inventory.Count)
-            //    {
-            //        Utils.WriteColorLine(Inventory[i].info.Name, ConsoleColor.White);
-            //    }
-            //    else
-            //    {
-            //        Utils.WriteColorLine("  나가기", ConsoleColor.Gray);
-            //    }
-            //}
-            //Utils.CursorMenu.Clear();
+            Console.SetCursorPosition(0, 10);
             if (Inventory.Count == 0)//아이템이 없다면 아무것도 없다는 문장과 엔터키를 누르면 나가는 기능
             {
                 Utils.WriteColorLine("현재 가방에 있는 것: 공기", ConsoleColor.DarkGray);
-                Utils.WriteColorLine("엔터 키 누르면 이전 메뉴로 돌아갑니다.", ConsoleColor.DarkGray);
-                var key = Console.ReadKey(true);
-                if (key.Key == ConsoleKey.Enter)
-                {
-                    Program.CurrentScene = new Scene_Lobby();
-                    
-                    return;
-                }
-            }
-            
-            // 인벤토리의 커서메뉴 추가
-            for (int i = 0; i < Inventory.Count; i++)
-            {
-                string displayName = (Inventory[i].info.IsEquipped ? "[E]" : "")+Inventory[i].info.Name ;//상태에 따라 표시할 이름 생성
-                var selectItem = Inventory[i];//캡쳐 방지
-                
                 Utils.CursorMenu.Add((
-                    displayName, // 메뉴에 출력될 아이템 이름
-                    () =>
-                    {
-                        Console.Clear();
-                        //장착 상태에 따라 토글
-                        if (selectItem.info.IsEquipped==false)
+                        "나가려면 Enter키를 누르세요.", // 메뉴에 출력될 아이템 이름
+                        () =>
                         {
-                            selectItem.info.IsEquipped = true;
+                            Program.CurrentScene = new Scene_Lobby();
                         }
-                        else
-                        {
-                            selectItem.info.IsEquipped = false;
-                        }
-                    }
                 ));
-                
-                
             }
-            Utils.CursorMenu.Add((
-                    "나가기",
-                    () =>
-                    {
-                        Program.CurrentScene = new Scene_Lobby();
-                    }
-            ));
-            Utils.DisplayCursorMenu(5, 5);
+            else
+            {
+                // 인벤토리의 커서메뉴 추가
+                for (int i = 0; i < Inventory.Count; i++)
+                {
+                    var selectItem = Inventory[i];//캡쳐 방지
+
+                    string displayName = (selectItem.Iteminfo.IsEquip ? "[E]" : "") + selectItem.Iteminfo.Name;//상태에 따라 표시할 이름 생성
+
+                    Utils.CursorMenu.Add((
+                        displayName, // 메뉴에 출력될 아이템 이름
+                        () =>
+                        {
+                            Console.Clear();
+                            //장착 상태에 따라 토글
+                            selectItem.SetBool(ItemStat.IsEquip);
+                        }
+                    ));
+
+
+                }
+                Utils.CursorMenu.Add((
+                        "나가기",
+                        () =>
+                        {
+                            Program.CurrentScene = new Scene_Lobby();
+                        }
+                ));
+            }
+            Utils.DisplayCursorMenu(5, 15);
             
         }
-        private void ItemSelect(Item selectedItem)//아이템 장착 메서드
-        {
-            Utils.WriteColorLine($"\n {selectedItem.info.Name}가 선택되었습니다.", ConsoleColor.DarkYellow);
-        }
+       
         
-        // 아래는 출력 시험하기 위해 추가한 코드
-        private void MakeItem()
-        {
-            Inventory.Add(SetItem("찌르기 클로"));
-            Inventory.Add(SetItem("롱기누스의 손가락"));
-            Inventory.Add(SetItem("강철 손가락"));
-        }
-
-        private Item SetItem(string name)
-        {
-
-            Item item = new Item() { info = new() { Name = name, Description = "", IsSold = true, Price = 100 } };
-            return item;
-        }
-
-        private void ResetCursorPosition(int left,int top)
-        {
-            Console.SetCursorPosition(left, top+1);
-        }
+       
     
     }
         
