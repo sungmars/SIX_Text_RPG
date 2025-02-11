034018a1c23d3034c0501397f1ce429075158e0f
sungmars <136216542+sungmars@users.noreply.github.com>
2025-02-11 15:22:30 +0900

    🔨<Refactor>: “아이템칸 분리”
    장비아이템과 소모아이템칸을 분리하였고 아이템리스트는 필터링하여 장비리스트와 소모아이템리스트로 분리함
    소모아이템은 count로 이름 중복시 개수 나타냄

--- SIX_Text_RPG/SIX_Text_RPG/Scenes/Scene_Inventory.cs
+++ SIX_Text_RPG/SIX_Text_RPG/Scenes/Scene_Inventory.cs
@@ -39,27 +39,40 @@ namespace SIX_Text_RPG.Scenes
             }
             else
             {
-                // 인벤토리의 커서메뉴 추가
-                for (int i = 0; i < Inventory.Count; i++)
+                // 장비아이템, 소모아이템 필터링
+                var equipItems = Inventory.Where(item => item is IEquipable).ToList();
+                var consumItems = Inventory.Where(item => item is IConsumable).ToList();
+                //장비 아이템 출력
+                Utils.WriteColorLine("장비가능한 아이템", ConsoleColor.DarkYellow);
+                for (int i = 0; i < equipItems.Count; i++)
                 {
-                    var selectItem = Inventory[i];//캡쳐 방지
-                    IEquipable? equipable = selectItem as IEquipable;
-                    IConsumable? consumable = selectItem as IConsumable;
-
-                    string displayName = (equipable != null && selectItem.Iteminfo.IsEquip ? "[E]" : "") 
-                    + selectItem.Iteminfo.Name;//상태에 따라 표시할 이름 생성
-                    
-                    Utils.CursorMenu.Add((
-                        displayName, // 메뉴에 출력될 아이템 이름
-                        () =>
-                        {
-                            Console.Clear();
-                            //장착 상태에 따라 토글
-                            UseItem(i);
-                        }
+                    var selectItem = equipItems[i];
+                    string displayName = selectItem.Iteminfo.Name +
+                        (selectItem.Iteminfo.IsEquip ? "[E]" : "");//장착상태 표시해주기
+                    Utils.CursorMenu.Add((displayName, () =>
+                    {
+                        Console.Clear();
+                        UseItem(Inventory.FindIndex(x=>x.Equals(selectItem)));
+                    }
                     ));
                 }
-                Utils.CursorMenu.Add(("나가기",() =>{Program.CurrentScene = new Scene_Lobby();}));
+                Utils.DisplayLine(true, 2);
+                //소비 아이템 출력할곳
+                for (int i = 0; i < consumItems.Count; i++)
+                {
+                    var selectItem = consumItems[i];
+                    string itemName = consumItems[i].Iteminfo.Name;
+                    int count = consumItems.Count(item => item.Iteminfo.Name == itemName);//중복개수 세기
+                    string displayName = selectItem.Iteminfo.Name +
+                        (count!=0 ? $" -({count})" : "이게 보인다면 버그입니다.");//아이템 갯수 표시
+                    Utils.CursorMenu.Add((displayName, () =>
+                    {
+                        Console.Clear();
+                        UseItem(Inventory.FindIndex(x => x.Equals(selectItem)));
+                    }
+                    ));
+                }
+                Utils.CursorMenu.Add(("나가기", () => { Program.CurrentScene = new Scene_Lobby(); }));
                 Utils.DisplayCursorMenu(5, 7);
             }
         }
@@ -69,22 +82,20 @@ namespace SIX_Text_RPG.Scenes
             IEquipable? equipable = selectItem as IEquipable;
             IConsumable? consumable = selectItem as IConsumable;
 
-            if (consumable != null)//소비하는 아이템이라면
+            if (consumable != null && consumable.Consume())//소비하는 아이템이라면
             {
-                consumable.Consume();//소비 메서드 호출
+                Inventory.Remove(selectItem);//인벤토리리스트에서 삭제
             }
-            else if (equipable != null)
+            else if (equipable != null)//장비하는 아이템이라면
             {
                 equipable.Equip();//장비 메서드호출
             }
             else
             {
-                Console.WriteLine("착용불가한 아이템");
+                Utils.WriteColorLine("착용불가한 아이템", ConsoleColor.Red);
                 Console.ReadKey(); // 대기
             }
         }
     }
 
 }
-
-
